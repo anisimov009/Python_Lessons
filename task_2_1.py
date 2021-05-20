e82a76d92791ad966c2e8c4d430a6cbc61c8bdcf
Anisimov009 <anisimov009@yandex.ru>
2021-05-20 11:55:10 +0300

    Основы языка Python, урок 2.

---
+++ task_2_1.py
@@ -0,0 +1,19 @@
+# 1. Выяснить тип результата выражений:
+# 15 * 3
+# 15 / 3
+# 15 // 2
+# 15 ** 2
+
+value = 15 * 3
+print('15 * 3 = ', value, type(value))
+
+value = 15 / 3
+print('15 / 3 = ', value, type(value))
+
+value = 15 // 3
+print('15 // 3 = ', value, type(value))
+
+value = 15 ** 2
+print('15 ** 2 = ', value, type(value))
+
+
